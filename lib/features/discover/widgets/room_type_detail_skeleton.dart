abb18bac46417d7da79c9578f3b6fb1d3fc6fa70
Nguyencaobach <caobach19112005@gmail.com>
2026-07-02 14:17:54 +0700

    Update widgets for Profile feature

--- lib/features/discover/widgets/room_type_detail_skeleton.dart
+++ lib/features/discover/widgets/room_type_detail_skeleton.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
-import '../../../core/theme/app_colors.dart';
 
 class RoomTypeDetailSkeleton extends StatelessWidget {
   const RoomTypeDetailSkeleton({super.key});
